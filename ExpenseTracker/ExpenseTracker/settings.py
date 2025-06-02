9f3fe15920cec701f3b4f3c53ca45ab0400375b0
widushan <114930601+widushan@users.noreply.github.com>
2025-06-02 10:54:20 +0530

    home commit
    after logging redirect to home

--- ExpenseTracker/ExpenseTracker/settings.py
+++ ExpenseTracker/ExpenseTracker/settings.py
@@ -123,4 +123,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = "home"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
