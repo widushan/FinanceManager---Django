eb1051be7004aa9b89d903dfbaf115bc183c6c3e
widushan <114930601+widushan@users.noreply.github.com>
2025-06-04 01:00:29 +0530

    final commit
    Project done

--- ExpenseTracker/exp_tracker/urls.py
+++ ExpenseTracker/exp_tracker/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/register/", views.register, name="register"),
     path("accounts/login/", views.register, name="login"),
-    path("accounts/logout/", views.register, name="logout"),
+    
     path("expenses", views.ExpenseListView.as_view(), name='expenses'),
     path("incomes", views.IncomeListView.as_view(), name='incomes'),
     path("expenses/edit/<int:expense_id>/", views.edit_expense, name='edit_expense'),
