d12054bb9034efa6a38db9166a25e18a2a22d4f2
widushan <114930601+widushan@users.noreply.github.com>
2025-08-20 15:16:08 +0530

    PostgreSQL commit
     PostgreSQL connected

--- ExpenseTracker/ExpenseTracker/settings.py
+++ ExpenseTracker/ExpenseTracker/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "ExpenseTracker.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'finance',
+        'USER': 'postgres',
+        'PASSWORD': 'postroot',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
